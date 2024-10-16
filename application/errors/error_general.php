005ba4f23db2fae78a922d9b59c8ef7197381e6f
Thanakij Pechprasarn <thanakij.p@foremost-ai.com>
2024-10-16 12:09:25 +0700

    refactor: richer error message for ENVIRONMENT === 'development'

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -1,3 +1,4 @@
 <?php
-echo json_encode(array("success" => false, "error_code" => "0800", "message" => "There is an internal server error"));
+const dev = defined('ENVIRONMENT') && ENVIRONMENT === 'development';
+echo json_encode(array("success" => false, "error_code" => "0800", "message" => (dev ? "error - $heading" : "There is an internal server error")));
 ?>
